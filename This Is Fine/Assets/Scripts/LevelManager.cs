7ff8e9388070a5819a3ba41732591a0c9fe32e97
mahyar babaie <mahyarhajibabaie@gmail.com>
2018-08-06 22:16:16 -0700

    Cleaned up the Unity Files and added MainMenu

---
+++ This Is Fine/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    // Loading the level by scene name
+    public void LoadLevel(string name)
+    {
+        // setting game to normal speed
+        Time.timeScale = 1;
+        SceneManager.LoadScene(name);
+
+    }
+
+    // Quit the game
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+}

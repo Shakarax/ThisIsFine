91de3ae3a058f821122ce5debb1d6dcf56bfffce
mahyar babaie <mahyarhajibabaie@gmail.com>
2018-08-26 15:30:42 -0700

    Added Minimap
    # Minimap changes based on waht level is loaded
    # Minimap follows player

---
+++ This Is Fine/Assets/Scripts/Utility/UI/Minimap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    private Transform target;
+    public Transform Target { get { return target; } set { target = value; } }
+
+    private void Start()
+    {
+        target = GameObject.FindObjectOfType<PlayerController>().transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = target.position;
+        transform.position = newPosition;
+    }
+}
